321d282303db6daf5914788a57ed2fe10f8a47ed
Noblesse Kasa <noblessekasa2@gmail.com>
2020-07-01 13:25:21 +0200

    issue-2B-inscription

--- src/main/java/com/mwdf/mwdf/controller/userController.java
+++ src/main/java/com/mwdf/mwdf/controller/userController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-//import sn.domain.Player;
-//import sn.persistence.PlayerRepository;
 
 @Controller
 public class userController
@@ -21,50 +21,20 @@ public class userController
 	@GetMapping("/inscription")
 	public String inscription(/*User user*/)
 	{
-		return "inscription"; //le nom du template
+		return "inscription";
 	}
 	
 	@GetMapping("/connexion")
 	public String connexion(/*User user*/)
 	{
-		return "connexion"; //le nom du template
+		return "connexion";
 	}
 	
-	/*@PostMapping("/saveUser")
-	public String savePlayer(@Valid User user,BindingResult result,Model model)
+	//@PostMapping("/saveUser")
+	@RequestMapping("/saveUser")
+	@ResponseBody
+	public String savePlayer(/*@Valid User user,BindingResult result,Model model*/)
 	{
-		userRepo.save(user);
-		model.addAttribute("users", userRepo.findAll()); // c'est ça qui affiche tous les utilisateur
-		return "index";
+		return "redirection to saveUser ok";
 	}
-	
-	@GetMapping("/updatePlayer/{playerId}")
-	public String updatePlayer(@PathVariable long playerId,Model model) throws Exception
-	{	
-		Player updatedPlayer=playerRepo.findById(playerId).orElseThrow(()->new Exception("player with ID "+playerId+"not found"));
-		model.addAttribute("updatedPlayer",updatedPlayer);
-		
-		return "update-player";
-	}
-	
-	@PostMapping("/saveUpdatedPlayer/{playerId}") 
-	public String saveChanges(@PathVariable long playerId,@Valid Player player,BindingResult result,Model model) throws Exception
-	{
-		Player updatedPlayer=playerRepo.findById(playerId).orElseThrow(()->new Exception("player with ID "+playerId+"not found"));
-		updatedPlayer.setTeam(player.getTeam());
-		updatedPlayer.setFirstname(player.getFirstname());
-		updatedPlayer.setSecondname(player.getSecondname());
-		updatedPlayer.setNumber(player.getNumber());
-		playerRepo.save(updatedPlayer);
-		model.addAttribute("players", playerRepo.findAll());
-		return "index";
-	}
-	
-	@DeleteMapping("/deletePlayer/{playerId}")
-	public String deletePlayer(@PathVariable long playerId,Model model) throws Exception
-	{	
-		Player deletedPlayer=playerRepo.findById(playerId).orElseThrow(()->new Exception("player with ID "+playerId+"not found"));
-		playerRepo.delete(deletedPlayer);
-		return "index";
-	}*/
 }
