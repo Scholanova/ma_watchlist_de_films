851b4532c18d50f1ed11fb9bd74ebeec3a943262
LivioChan <ctlivio@yahoo.fr>
2020-07-03 13:06:04 +0200

    update entity movie and added result entity

--- src/main/java/com/mwdf/mwdf/entity/Movie.java
+++ src/main/java/com/mwdf/mwdf/entity/Movie.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class Movie {
 	
+
 	private boolean adult;
 	private long budget;
 	private int id;
@@ -13,15 +14,15 @@ public class Movie {
 	private String original_title;
 	private String overview;
 	private double popularity;
-	
-	//JSON
+	private String poster_path;
+	private String backdrop_path;
 	
 	public Movie() {
 		
 	}
 	
 	public Movie(boolean adult, long budget, int id, String imdb_id, String original_language, String original_title,
-			String overview, double popularity) {
+			String overview, double popularity, String poster_path, String backdrop_path) {
 		super();
 		this.adult = adult;
 		this.budget = budget;
@@ -31,6 +32,8 @@ public class Movie {
 		this.original_title = original_title;
 		this.overview = overview;
 		this.popularity = popularity;
+		this.poster_path = poster_path;
+		this.backdrop_path = backdrop_path;
 	}
 	
 	public boolean isAdult() {
@@ -81,11 +84,34 @@ public class Movie {
 	public void setPopularity(double popularity) {
 		this.popularity = popularity;
 	}
+
+	public String getPoster_path() {
+		return poster_path;
+	}
+
+	public void setPoster_path(String poster_path) {
+		this.poster_path = poster_path;
+	}
+
+	public String getBackdrop_path() {
+		return backdrop_path;
+	}
+
+	public void setBackdrop_path(String backdrop_path) {
+		this.backdrop_path = backdrop_path;
+	}
+
 	@Override
 	public String toString() {
 		return "Movie [adult=" + adult + ", budget=" + budget + ", id=" + id + ", imdb_id=" + imdb_id
 				+ ", original_language=" + original_language + ", original_title=" + original_title + ", overview="
-				+ overview + ", popularity=" + popularity + "]";
+				+ overview + ", popularity=" + popularity + ", poster_path=" + poster_path + ", backdrop_path="
+				+ backdrop_path + "]";
+	}
+	
+	public String getAffiche() {
+		
+		return  "https://image.tmdb.org/t/p/original" + this.getPoster_path();
 	}
 	
 }
