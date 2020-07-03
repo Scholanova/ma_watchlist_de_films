4a40df3dc523822612a6f93c14050451942bdc52
LivioChan <ctlivio@yahoo.fr>
2020-07-03 13:06:04 +0200

    update entity movie and added result entity

--- src/main/java/com/mwdf/mwdf/services/MovieService.java
+++ src/main/java/com/mwdf/mwdf/services/MovieService.java
@@ -7,11 +7,16 @@ public interface MovieService {
 
 
     //public Movie searchMovie(String params) throws MovieException;
+<<<<<<< mwdf-Issue-2A
 	public String getMovie(int movieId);
     public String searchMovies(String params);
     public String getUrlContent(String lien);
     public String getLatestMovie();
     public String getRandomMovie();
     
+=======
+    public String searchMovies(String params);
+    public String getUrlContent(String lien);
+>>>>>>> update entity movie and added result entity
 
 }
