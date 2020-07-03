91e42f60c085ef8e454c897fd08365e90bd35f23
Your Name <eva.sobaco.morison@gmail.com>
2020-07-03 10:09:06 +0200

    Ajout des migrations tables lists movies et contraintes fk

--- src/main/resources/db/schema/004_add_constraints.sql
+++ src/main/resources/db/schema/004_add_constraints.sql
@@ -1,25 +1,30 @@
 <<<<<<< mwdf-Issue-2A
 <<<<<<< mwdf-Issue-2A
 <<<<<<< mwdf-Issue-2A
+<<<<<<< mwdf-Issue-2A
 --liquibase formatted sql
 
 =======
 >>>>>>> ajout des contraintes migrations
 --changeset scholanova:5
+=======
+--liquibase formatted sql
+>>>>>>> Ajout des migrations tables lists movies et contraintes fk
 
-ALTER TABLE USERS ADD COLUMN LIST_ID;
+--changeset scholanova:5
+ALTER TABLE USERS ADD COLUMN LIST_ID INTEGER;
 ALTER TABLE USERS
 ADD CONSTRAINT constraint_user FOREIGN KEY (LIST_ID) REFERENCES LISTS (LIST_ID);
 
-ALTER TABLE LISTS ADD COLUMN MOVIE_ID;
+ALTER TABLE LISTS ADD COLUMN MOVIE_ID INTEGER;
 ALTER TABLE LISTS
 ADD CONSTRAINT constraint_list FOREIGN KEY (MOVIE_ID) REFERENCES MOVIES (MOVIE_ID);
 
-ALTER TABLE LISTS ADD COLUMN USER_ID;
+ALTER TABLE LISTS ADD COLUMN USER_ID INTEGER;
 ALTER TABLE LISTS
 ADD CONSTRAINT constraint_list2 FOREIGN KEY (USER_ID) REFERENCES USERS (USER_ID);
 
-ALTER TABLE MOVIES ADD COLUMN LIST_ID;
+ALTER TABLE MOVIES ADD COLUMN LIST_ID INTEGER;
 ALTER TABLE MOVIES
 <<<<<<< mwdf-Issue-2A
 <<<<<<< mwdf-Issue-2A
