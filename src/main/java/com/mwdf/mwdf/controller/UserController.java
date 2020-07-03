650e72130fee019782029f13fcb6e445d88b0e95
Your Name <eva.sobaco.morison@gmail.com>
2020-07-03 11:57:53 +0200

    creation UserController

---
+++ src/main/java/com/mwdf/mwdf/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.mwdf.mwdf.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mwdf.mwdf.entity.Movie;
+import com.mwdf.mwdf.models.User;
+import com.mwdf.mwdf.repositories.UserRepository;
+import com.mwdf.mwdf.utils.RoleEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
+
+@Controller
+@EnableAutoConfiguration
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping("/user/{username}")
+    public String getUserInformation(@PathVariable("username") String username) {
+
+    }
+
+}
