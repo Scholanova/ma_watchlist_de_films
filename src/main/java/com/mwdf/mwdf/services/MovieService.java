91ad5d465a0e9d427a68880f4febdba54693fe42
LivioChan <ctlivio@yahoo.fr>
2020-07-07 10:00:30 +0200

    etape 5 route + movie service

--- src/main/java/com/mwdf/mwdf/services/MovieService.java
+++ src/main/java/com/mwdf/mwdf/services/MovieService.java
@@ -1,17 +1,18 @@
 package com.mwdf.mwdf.services;
 
 import com.mwdf.mwdf.entity.Movie;
+import com.mwdf.mwdf.entity.Result;
 import com.mwdf.mwdf.exception.MovieException;
 
 public interface MovieService {
 
 
     //public Movie searchMovie(String params) throws MovieException;
-	public String getMovie(int movieId);
-    public String searchMovies(String params);
+	public Movie getMovie(int movieId);
+    public Result searchMovies(String params);
     public String getUrlContent(String lien);
-    public String getLatestMovie();
-    public String getRandomMovie();
+    public Movie getLatestMovie();
+    public Movie getRandomMovie();
     
 
 }
--- src/main/java/com/mwdf/mwdf/services/MovieServiceImpl.java
+++ src/main/java/com/mwdf/mwdf/services/MovieServiceImpl.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mwdf.mwdf.entity.Movie;
 import com.mwdf.mwdf.entity.Result;
-import com.mwdf.mwdf.exception.MovieException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,46 +47,43 @@ public class MovieServiceImpl implements MovieService{
 
         return sb.toString();
     }
-    public String getMovie(int movieId) {
-    	String url = "https://api.themoviedb.org/3/movie/"+ movieId+"?api_key="+TOKEN+"&language="+LANG;	
-    	return getUrlContent(url);
+    public Movie getMovie(int movieId) {
+    	String url = "https://api.themoviedb.org/3/movie/"+ movieId+"?api_key="+TOKEN+"&language="+LANG;
+    	String json = getUrlContent(url);
+    	return parseJsonToMovie(json);
     }
-    /*
-    public Movie searchMovie(@PathVariable("params") String params) throws MovieException {
-        String url = "https://api.themoviedb.org/3/search/movie?api_key="+TOKEN+"&query="+params;
-        String s = getUrlContent(url);
-        Movie m = new Movie();
+
+    public Result searchMovies(String params) {
+    	String url = "https://api.themoviedb.org/3/search/movie?api_key="+TOKEN+"&query="+params;
+        String json = getUrlContent(url);
+        Result res = new Result();
+
         try {
             ObjectMapper mapper = new ObjectMapper();
-            m = mapper.readValue(s, Movie.class);
-        } catch (JsonProcessingException e)
-        {
+            res = mapper.readValue(json, Result.class);
+
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        return m;
 
-    }
-    */
-    public String searchMovies(String params) {
-    	String url = "https://api.themoviedb.org/3/search/movie?api_key="+TOKEN+"&query="+params;
-        return getUrlContent(url);
+        return res;
     }
     public String search(@PathVariable("params") String params) {
         String url = "https://api.themoviedb.org/3/search/movie?api_key="+TOKEN+"&query="+params;
         return getUrlContent(url);
     }
-	public String getLatestMovie() {
+	public Movie getLatestMovie() {
 		String url = "https://api.themoviedb.org/3/movie/latest?api_key="+TOKEN+"&language="+LANG;
-		return getUrlContent(url);
+        String json = getUrlContent(url);
+        return parseJsonToMovie(json);
 	}
-	public String getRandomMovie() {
-		String jsonLatestMovie = getLatestMovie();
-		Movie latest = parseJsonToMovie(jsonLatestMovie);
+	public Movie getRandomMovie() {
+		Movie latest = getLatestMovie();
 		int idLatestMovie = latest.getId();
 		int randomMovieId = (int) (Math.random() * ( idLatestMovie - 100 ));
 		return getMovie(randomMovieId);
 	}
-	private Movie parseJsonToMovie(String json) {		
+	public Movie parseJsonToMovie(String json) {
 		Movie m = new Movie();
 		try {
 			ObjectMapper mapper = new ObjectMapper();
