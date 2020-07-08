a33c4c4e240941285662ac7f4f794c11c8a5b3d2
Cyril VUILLEMIN <cyril.vuillemin@hotmail.fr>
2020-07-08 12:14:15 +0200

    create model comment + repo + repotest

--- src/test/java/com/mwdf/mwdf/repositories/UserRepositoryTest.java
+++ src/test/java/com/mwdf/mwdf/repositories/UserRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.mwdf.mwdf.repositories;
 
+import com.mwdf.mwdf.models.Comment;
 import com.mwdf.mwdf.models.CustomList;
+import com.mwdf.mwdf.models.Movie;
 import com.mwdf.mwdf.models.User;
 import com.mwdf.mwdf.utils.RoleEnum;
 import org.junit.jupiter.api.AfterEach;
@@ -28,11 +30,14 @@ public class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @AfterEach
     void cleanUp() {
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "ROLES", "USERSLISTS", "MOVIESLISTS", "USERS", "LISTS", "MOVIES");
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "ROLES", "USERSLISTS", "MOVIESLISTS", "COMMENTS", "USERS", "LISTS", "MOVIES");
     }
 
     @Nested
@@ -67,6 +72,48 @@ public class UserRepositoryTest {
             // Then
             assertThat(userRepository.findByUsername(user.getUsername()).getIdUser()).isEqualTo(user.getIdUser());
 
+        }
+
+        @Test
+        void whenUser_thenReturnsUsersListsContainThisList() {
+            // Given
+            User user = new User("dzeq", "azaz", "cy", "ril", Collections.singleton(RoleEnum.USER));
+            Movie movie = new Movie(1);
+            Movie movie2 = new Movie(2);
+            CustomList list = new CustomList("maList");
+
+            list.getUsers().add(user);
+            user.getLists().add(list);
+            list.getMovies().add(movie);
+            list.getMovies().add(movie2);
+
+            Comment comment = new Comment("Super film");
+            comment.setUser(user);
+            comment.setMovie(movie);
+
+            Comment comment2 = new Comment("Nul film");
+            comment2.setUser(user);
+            comment2.setMovie(movie2);
+
+            Comment comment3 = new Comment("pas fou film");
+            comment3.setUser(user);
+            comment3.setMovie(movie);
+
+            user.getComment().add(comment);
+            user.getComment().add(comment2);
+            user.getComment().add(comment3);
+            movie.getComment().add(comment);
+            movie.getComment().add(comment3);
+            movie2.getComment().add(comment2);
+
+            userRepository.save(user);
+            commentRepository.save(comment);
+
+            // When
+            // Then
+            assertThat(commentRepository.findByUserAndMovie(user, movie2).get(0).getContent()).isEqualTo("Nul film");
+            assertThat(commentRepository.findByUserAndMovie(user, movie).size()).isEqualTo(2);
+            assertThat(commentRepository.findByUserAndMovie(user, movie2).size()).isEqualTo(1);
         }
     }
 }
